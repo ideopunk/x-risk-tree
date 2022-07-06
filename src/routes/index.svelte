f2ec5d16f226c9632ec0007b8c44e3a87c04f756
Conor Barnes <conorbarnes93@gmail.com>
2022-07-06 10:19:42 -0300

    data import function

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,21 @@
+<script lang="ts">
+	export let vals: {
+		total: number;
+		climate: number;
+		climateX: number;
+		nano: number;
+		nanoX: number;
+		nuke: number;
+		nukeX: number;
+		ai: number;
+		aiX: number;
+		bio: number;
+		bioX: number;
+	};
+
+	console.log({ vals });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>{Object.entries(vals)}</p>
