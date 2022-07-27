250be27008b9763c0387ec12d134da3532dc02f0
Conor Barnes <conorbarnes93@gmail.com>
2022-07-27 08:43:33 -0400

    new og image: tree sample

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,10 +9,10 @@
 
 	<meta name="og:title" content="The X-Risk Tree" />
 	<meta name="og:description" content="In how many of our branching paths does humanity survive?" />
-	<meta name="og:url" content="https://x-risk-tree.vercel.app/" />
+	<meta name="og:url" content="https://x-risk-tree.com/" />
 	<meta name="og:type" content="website" />
 
-	<meta name="og:image" content="https://x-risk-tree.vercel.app/tree.png" />
+	<meta name="og:image" content="https://x-risk-tree.come/tree-sample.png" />
 	<meta name="og:image:alt" content="A tree symbolizing humanity's possible futures" />
 	<meta name="og:image:width" content="773" />
 	<meta name="og:image:height" content="773" />
